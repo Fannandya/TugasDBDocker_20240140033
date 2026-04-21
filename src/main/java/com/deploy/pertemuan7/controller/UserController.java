a8fe772cb34d3426cea5e865aa1e5e32bb72a196
Fannandya Sutan <sfannandya@gmail.com>
2026-04-21 10:37:00 +0700

    menghapus import tidak dipakai di UserController

--- src/main/java/com/deploy/pertemuan7/controller/UserController.java
+++ src/main/java/com/deploy/pertemuan7/controller/UserController.java
@@ -2,7 +2,6 @@ package com.deploy.pertemuan7.controller;
 
 import com.deploy.pertemuan7.model.User;
 import com.deploy.pertemuan7.service.UserService;
-import org.springframework.boot.webmvc.autoconfigure.WebMvcProperties;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
